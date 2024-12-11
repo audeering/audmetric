5016444947ca4ca7c2458a1a23ceee874dd04a7c
ChristianGeng <christian.c.geng@gmail.com>
2024-12-11 10:35:03 +0100

    pep-format code.

--- audmetric/core/api.py
+++ audmetric/core/api.py
@@ -1202,7 +1202,7 @@ def word_error_rate(
     as the edit distance divided by a normalization factor n.
     This represents the average editing cost per sequence item.
     The value of n depends on the ``norm`` parameter.
-    
+
     If ``norm`` is ``"truth"``,
     n is set to the reference (truth) length,
     following the Wikipedia formulation.
